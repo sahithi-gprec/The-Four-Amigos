d9a89104328ba5c5733d63a6d4ef34950dc91c8b
sahithi <97873013+sahithi-gprec@users.noreply.github.com>
2023-01-16 15:54:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from game import Game
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+def main():
+    pygame.init()
+    # Set the width and height of the screen [width, height]
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    # Set the current window caption
+    pygame.display.set_caption("PACMAN")
+    #Loop until the user clicks the close button.
+    done = False
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    # Create a game object
+    game = Game()
+    while not done:
+        done = game.process_events()
+        game.run_logic()
+        game.display_frame(screen)
+        clock.tick(30)
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
